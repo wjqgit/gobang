02c842232968e5b0cbcc4ec942efeeacef58cf2d
wjq <385252460@qq.com>
2015-09-09 18:20:16 +0800

    add chat-example

--- chat-example/index.js
+++ chat-example/index.js
@@ -1,10 +1,23 @@
 var app = require('express')(),
-	http = require('http').Server(app)
+	http = require('http').Server(app),
+	io = require('socket.io')(http);
 
 app.get('/', (function(req, res){
-	res.send('<h1>Hello world</h1>')
-}))
+	res.sendFile(__dirname + '/index.html');
+}));
 
-http.listen(3000, (function(){
-	console.log('Server running on port 3000');
-}))
+io.on('connection', function(socket) {
+	console.log('a user connected...');
+	socket.broadcast.emit('chat message', "a new user connected");
+	socket.on('disconnect', function() {
+		console.log('user disconnected...');
+	});
+	socket.on('chat message', function(msg) {
+		// console.log('message: ' + msg);
+		socket.broadcast.emit('chat message', msg);
+	});
+});
+
+http.listen(3000, function() {
+	console.log('http server running on port 3000...');
+});
