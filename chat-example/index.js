8ac61701aae463f9932e163dede0b53ab3d6daf1
wjq <385252460@qq.com>
2015-09-09 20:04:46 +0800

    change index.js

--- chat-example/index.js
+++ chat-example/index.js
@@ -2,19 +2,22 @@ var app = require('express')(),
 	http = require('http').Server(app),
 	io = require('socket.io')(http);
 
+var user_count = 0;
+
 app.get('/', (function(req, res){
 	res.sendFile(__dirname + '/index.html');
 }));
 
 io.on('connection', function(socket) {
 	console.log('a user connected...');
-	socket.broadcast.emit('chat message', "a new user connected");
+	socket.emit('chat message', "you are user #" + (++user_count))
+	socket.broadcast.emit('chat message', "user #" + user_count + " connected...");
 	socket.on('disconnect', function() {
 		console.log('user disconnected...');
 	});
 	socket.on('chat message', function(msg) {
 		// console.log('message: ' + msg);
-		socket.broadcast.emit('chat message', msg);
+		io.emit('chat message', msg);
 	});
 });
 
