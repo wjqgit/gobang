c7d40d8e0d65947f0f65f3f922720df15fad19a2
wjq <385252460@qq.com>
2015-09-17 18:32:58 +0800

    start working on visualization of warehouse

---
+++ graphics/kovan/server.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+	app = express(),
+	server = require('http').Server(app),
+	io = require('socket.io')(server);
+
+var port = 9000;
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/:tagId', function(req, res) {
+	res.sendFile(__dirname + '/' + req.params.tagId + '.html');
+});
+
+server.listen(port, function() {
+	console.log('Server running on port: ' + port);
+});
