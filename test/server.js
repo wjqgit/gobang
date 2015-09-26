abae307784bae1f0571ce9ef2a85e1c063a9d834
wjqgit <wjq510@gmail.com>
2015-09-27 00:02:03 +0800

    modify test

--- test/server.js
+++ test/server.js
@@ -2,5 +2,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(3000, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:8080/');
+}).listen(3000, function() {
+	console.log('Server running at port 3000');	
+});
