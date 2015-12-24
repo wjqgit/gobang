7bb66e44b59b1dad118071e50e1c9ec50ec22386
wjq <385252460@qq.com>
2015-12-24 15:44:12 +0800

    minor fix

--- test/worker.js
+++ test/worker.js
@@ -1,10 +1,9 @@
 var n = 1;
-
 search: while (true) {
   n += 1;
   for (i = 2; i <= Math.sqrt(n); i++) {
     if (n % i == 0)
-    continue search;
+      continue search;
 
     postMessage(n);
   }
