2878e8f7009401229d795fabc5993c94cb9f6585
wjq <wang.jiaqi@kovansystems.com>
2016-11-30 18:31:53 +0800

    Added mobx-three.

---
+++ sandbox/mobx-three/src/js/index.js
@@ -0,0 +1,12 @@
+import * as THREE from 'three'
+import Stats from 'stats.js'
+
+window.THREE = THREE
+window.Stats = Stats
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const app = document.getElementById('app')
+
+ReactDOM.render(<div />, app)
