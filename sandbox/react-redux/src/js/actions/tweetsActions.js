d30b99fdf40085f3b439efa5c67b38a0c45e1f71
wjq <wjq510@gmail.com>
2017-02-10 20:43:23 +0800

    Test use axios to get json api.

--- sandbox/react-redux/src/js/actions/tweetsActions.js
+++ sandbox/react-redux/src/js/actions/tweetsActions.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 
 export function fetchTweets() {
   return function(dispatch) {
-    axios.get('http://rest.learncode.academy/api/test123/tweets')
+    axios.get('https://codepen.io/jobs.json')
+    // axios.get('http://rest.learncode.academy/api/test123/tweets')
     .then(res => dispatch({type: 'FETCH_TWEETS_FULFILLED', payload: res.data}))
     .catch(err => dispatch({type: 'FETCH_TWEETS_REJECTED', payload: err}))
   }
